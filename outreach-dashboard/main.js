bcb85482e1c58e467ef06d2a66d8953c7118afcb
Leo <leo@flextail.com>
2026-05-25 19:55:43 +0800

    feat: add Electron app wrapper + desktop launcher

---
+++ outreach-dashboard/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1024,
+    minHeight: 700,
+    title: 'Outreach Dashboard',
+    icon: path.join(__dirname, 'icon.svg'),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile('index.html');
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
